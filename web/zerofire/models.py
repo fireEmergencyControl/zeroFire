654ca2816dad63a41770406e73d412574a055df7
deepDark2 <shimhotdog@naver.com>
2022-05-12 14:18:50 +0900

    fix model and register--회원가입 및 데이터베이스 구축

--- web/zerofire/models.py
+++ web/zerofire/models.py
@@ -2,22 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Manager(models.Model):
-    mno = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=10, blank=True, null=True)
-    email = models.CharField(max_length=100, blank=True, null=True)
-    id = models.CharField(db_column='ID', max_length=20, blank=True, null=True)  # Field name made lowercase.
-    pass_field = models.CharField(db_column='pass', max_length=20, blank=True, null=True)  # Field renamed because it was a Python reserved word.
-    rno = models.ForeignKey('Rankdata', models.DO_NOTHING, db_column='rno', blank=True, null=True)
+    mno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10, blank=True)
+    email = models.CharField(max_length=100, blank=True)
+    workarea = models.CharField(max_length=25)
+    id = models.CharField(db_column='ID', max_length=20)  # Field name made lowercase.
+    pass_field = models.CharField(db_column='pass', max_length=20)  # Field renamed because it was a Python reserved word.
+    rno = models.ForeignKey('Rankdata', models.DO_NOTHING, db_column='rno')
 
     class Meta:
-        managed = True
         db_table = 'manager'
 
 
 class Rankdata(models.Model):
     rno = models.IntegerField(primary_key=True)
-    rankdata = models.CharField(max_length=10, blank=True, null=True)
+    rankdata = models.CharField(max_length=10, blank=True)
 
     class Meta:
-        managed = True
         db_table = 'rankdata'
